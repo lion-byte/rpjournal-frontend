36356247eb7ddeb426400f55d96f6516ab4d1e27
Mark Hernandez <mark@lion-byte.com>
2019-02-02 13:39:44 -0600

    Add the update and delete operations

--- src/backend/src/resolvers/mutation.js
+++ src/backend/src/resolvers/mutation.js
@@ -7,38 +7,89 @@ const Mutation = {
     return db.mutation.createAdventure({ data: { title, description } }, info)
   },
 
-  async createSession (parent, args, ctx, info) {
-    const { id, title, description } = args
+  async updateAdventure (parent, args, ctx, info) {
+    const { id, ...updates } = args
 
-    const adventure = await db.query.adventure({ where: { id } })
-
-    if (!adventure) {
-      throw Error(`No such Adventure for ${id}`)
-    }
-
-    return db.mutation.createSession(
+    return db.mutation.updateAdventure(
       {
-        data: { title, description, adventure: { connect: { id } } }
+        data: updates,
+        where: { id }
       },
       info
     )
   },
 
-  async createQuest (parent, args, ctx, info) {
-    const { id, title, description, completed = false } = args
+  async deleteAdventure (parent, args, ctx, info) {
+    const { id } = args
 
-    const adventure = await db.query.adventure({ where: { id } })
+    return db.mutation.deleteAdventure({ where: { id } }, info)
+  },
+
+  async createSession (parent, args, ctx, info) {
+    const { id: adventureId, title, description } = args
+
+    const adventure = await db.query.adventure({ where: { id: adventureId } })
 
     if (!adventure) {
-      throw Error(`No such Adventure for ${id}`)
+      throw Error(`No such Adventure for ${adventureId}`)
+    }
+
+    return db.mutation.createSession(
+      {
+        data: {
+          title,
+          description,
+          adventure: { connect: { id: adventureId } }
+        }
+      },
+      info
+    )
+  },
+
+  async updateSession (parent, args, ctx, info) {
+    const { id, ...updates } = args
+
+    return db.mutation.updateSession({ data: updates, where: { id } }, info)
+  },
+
+  async deleteSession (parent, args, ctx, info) {
+    const { id } = args
+
+    return db.mutation.deleteSession({ where: { id } }, info)
+  },
+
+  async createQuest (parent, args, ctx, info) {
+    const { id: adventureId, title, description, completed = false } = args
+
+    const adventure = await db.query.adventure({ where: { id: adventureId } })
+
+    if (!adventure) {
+      throw Error(`No such Adventure for ${adventureId}`)
     }
 
     return db.mutation.createQuest(
       {
-        data: { title, description, completed, adventure: { connect: { id } } }
+        data: {
+          title,
+          description,
+          completed,
+          adventure: { connect: { id: adventureId } }
+        }
       },
       info
     )
+  },
+
+  async updateQuest (parent, args, ctx, info) {
+    const { id, ...updates } = args
+
+    return db.mutation.updateQuest({ data: updates, where: { id } }, info)
+  },
+
+  async deleteQuest (parent, args, ctx, info) {
+    const { id } = args
+
+    return db.mutation.deleteQuest({ where: { id } }, info)
   }
 }
 
